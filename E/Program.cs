93c46353efb3430ed220feac40af0d4c890b4adc
Orlando William <0rlandw@gmail.com>
2016-08-05 23:55:06 -0400

    first working version

--- E/Program.cs
+++ E/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,40 @@ namespace E
 {
     class Program
     {
+        static string Exec(string proc, string args)
+        {
+            var info = new ProcessStartInfo
+            {
+                FileName = proc,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                Arguments = args
+            };
+
+            return Process.Start(info)
+                .StandardOutput.ReadToEnd()
+                .Trim();
+        }
+
+        static string Emacs(string args) => Exec("emacsclient", $"-n -a \"runemacs\" {args}");
+
+        static void Focus()
+        {
+            const string focus = "(select-frame-set-input-focus (selected-frame))";
+            const string maximize = "(set-frame-parameter nil 'fullscreen 'maximized)";
+
+            Emacs($"-e \"(progn {focus} {maximize})\"");
+        }
+
         static void Main(string[] args)
         {
+            var arguments = (args.Any())
+                ? string.Join(" ", args)
+                : "-c";
+
+            Emacs(arguments);
+            Focus();
         }
     }
 }
--- E/Properties/AssemblyInfo.cs
+++ E/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("E")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("emacs launcher")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("E")]
